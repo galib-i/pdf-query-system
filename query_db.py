352d9b0de33026842058ea2a95db5ecb4bc18b7c
Galib <60702760+galib-i@users.noreply.github.com>
2024-09-11 16:42:02 +0100

    Move from CLI to Streamlit

--- query_db.py
+++ query_db.py
@@ -1,4 +1,4 @@
-import argparse
+import streamlit as st
 
 from langchain.prompts import ChatPromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
@@ -6,23 +6,47 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 from utils import initialise_embeddings, initialise_chroma
 
 PROMPT_TEMPLATE = """
-Answer the question based only on the following context, if you cannot answer, say "Answer not found":
+Answer the question based only on the following context, from the given files:
 
 {context}
 
 ---
 
-Answer the question based on the above context: {question}
+Answer the question based on the above context, from the given files: {question}
 """
 
 
 def main():
-    """Parses the CLI query to perform a RAG search"""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("query_text", type=str, help="The query text.")
-    args = parser.parse_args()
+    """Run the Streamlit program to query PDFs using the RAG model"""
+    st.set_page_config(page_title="Query PDFs", page_icon="💬")
+    st.title("Query your PDF documents")
 
-    query_rag(args.query_text)
+    if "messages" not in st.session_state.keys():  # initialises the chat history
+        st.session_state.messages = [{
+            "role": "ai",
+            "content": "Ask me a question about your documents!"
+        }]
+
+    if query := st.chat_input("Ask a question"):  # saves input to chat history
+        st.session_state.messages.append({"role": "user", "content": query})
+
+    for message in st.session_state.messages:  # displays chat history
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if st.session_state.messages[-1]["role"] != "ai":  # if the user has asked a question
+        with st.chat_message("ai"):  # AI responds to the user's question
+            response_text, sources = query_rag(st.session_state.messages[-1]["content"])
+
+            output = f"""
+            {response_text}
+
+            Scanned: {sources}
+            """
+            st.write(output)
+            # saves the AI response to the chat history
+            message = {"role": "assistant", "content": output}
+        st.session_state.messages.append(message)
 
 
 def query_rag(query_text):
@@ -34,10 +58,14 @@ def query_rag(query_text):
     # extracts content and their assigned scores from the found results
     context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
 
-    prompt = generate_prompt(context=context_text, question=query_text)
-    response_text, sources = return_response(prompt=prompt, results=results)
+    prompt_text = generate_prompt(context=context_text, question=query_text)
 
-    print(f"Response: {response_text}\nScanned: {sources}")
+    try:
+        response_text, sources = return_response(prompt=prompt_text, results=results)
+        return response_text, sources
+
+    except Exception as e:
+        return f"An error occurred: {e}"
 
 
 def generate_prompt(context, question):
@@ -54,6 +82,7 @@ def return_response(prompt, results):
 
     # lists the sources used to formulate the response
     sources = {doc.metadata.get("source", None) for doc, _score in results}
+    sources = ", ".join(sources)
 
     return response.content, sources
 
